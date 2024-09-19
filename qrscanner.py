c07db6eeecb523baaf4f82a16f617e6551b82813
Anjanadipti <anjanadipti848134@gmail.com>
2024-09-19 14:11:13 +0530

    Update qrscanner.py

--- qrscanner.py
+++ qrscanner.py
@@ -1,7 +1,4 @@
 import qrcode
-from pyzbar.pyzbar import decode
-from PIL import Image
+
 myqr = qrcode.make("https://www.youtube.com/watch?v=LhSv6FH9g4o")
-myqr.save("myqr.png",scale = 8)
-b = decode(Image.open("myqr.png"))
-print(b[0].data.decode("ascii"))
+myqr.save("myqr.png")
